36450538c2df76f003a813b6185ae0855dd15398
SethKalkstein <sethkal7@yahoo.com>
2019-11-22 10:58:30 -0500

    file io async, dir io sync and async

--- filer.js
+++ filer.js
@@ -1,6 +1,30 @@
 const fs = require("fs");
 
-const fileContents = fs.readFileSync("sampleText.txt", "utf8");
+/* const fileContents = fs.readFileSync("sampleText.txt", "utf8");
 
 fs.writeFileSync("writeToNewFile.txt", fileContents);
+ */
 
+const fileToRead = "sampleText.txt";
+const encoding = "utf8";
+
+/* const fileToWrite = "thirdFileWrite.txt";
+const fileToDelete = "secondWriteFile.txt"; */
+
+const fileToWrite = "secondWriteFile.txt";
+const fileToDelete = "thirdFileWrite.txt";
+
+fs.readFile(fileToRead, encoding, (err, data) => 
+    fs.writeFile(fileToWrite, data , (err) => { 
+        if(err) throw err;
+        console.log(`The file written was: ${fileToWrite}\nThe contents written to the file were:\n"\n${data}\n"`);
+        fs.unlink(fileToDelete, err2 => { 
+            if(err) throw err;
+            console.log(`Just Deleted: "${fileToDelete}"`)
+        });
+    })
+);
+
+console.log("Are we blocking?")
+
+// fs.unlink(fileToDelete, err => console.log(`Just Deleted: "${fileToDelete}"`));
