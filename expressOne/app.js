66e5da68b1179b87a2506491afd9c41ce1057e6e
SethKalkstein <sethkal7@yahoo.com>
2019-11-27 20:47:19 -0500

    finally got partials working, new syntax not backwards compatible

--- expressOne/app.js
+++ expressOne/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+// const path = require("path");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+// app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res)=> {
     res.sendfile(__dirname+"/index.html");
